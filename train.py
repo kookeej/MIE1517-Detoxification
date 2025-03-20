718136ab08398d63e4e7d6165e58927c63b420b9
kookeej <hjkook@g.skku.edu>
2025-03-20 15:32:58 +0900

    add llm train code

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+from pathlib import Path
 from tqdm import tqdm
 from typing import Optional
 from nltk.translate.bleu_score import corpus_bleu
@@ -14,23 +15,33 @@ from transformers import (
     AutoModelForCausalLM,
     get_linear_schedule_with_warmup
 )
+from peft import LoraConfig, get_peft_model, PeftModel
 
 from utils import set_randomness
 
 
-def train_one_epoch(model, dataloader, optimizer, scheduler, criterion, device):
+def train_one_epoch(model, tokenizer, dataloader, optimizer, scheduler, criterion, device):
     # train
     model.train()
     for idx, batch in enumerate(tqdm(dataloader)):
+        
         optimizer.zero_grad()
-        outputs = model(**batch)
         
-        logits = outputs.logits
-        loss = criterion(
-            logits.view(-1, logits.size(-1)),
-            batch['labels'].view(-1)
-        )
-        
+        if isinstance(model, T5ForConditionalGeneration):
+            outputs = model(**batch)
+            
+            logits = outputs.logits
+            loss = criterion(
+                logits.view(-1, logits.size(-1)),
+                batch['labels'].view(-1)
+            )
+        else:
+            labels = batch['labels']
+            labels[labels == tokenizer.pad_token_id] = -100
+            outputs = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=labels)
+            loss = outputs.loss
+
+
         loss.backward()
         optimizer.step()
         scheduler.step()
@@ -43,7 +54,6 @@ def valid_one_epoch(model, dataloader, tokenizer, raw_data, device):
     
     with torch.no_grad():
         for idx, batch in enumerate(tqdm(dataloader)):
-            
             outputs = model.generate(
                 **batch,
                 max_new_tokens=64,
@@ -129,6 +139,21 @@ class ParadetoxDatasetForTrain(Dataset):
                     truncation=True,
                     return_tensors='pt',
             )
+        else:
+            inputs = self.tokenizer(
+                "Your task is to review the given toxic comment and convert it into a polite, neutral sentence.\nToxic comment: " + self.data[idx]['toxic'],
+                max_length=self.max_length,
+                padding='max_length',
+                truncation=True,
+                return_tensors='pt',
+            )
+            targets = self.tokenizer(
+                self.data[idx]['neutral'],
+                max_length=self.max_length,
+                padding='max_length',
+                truncation=True,
+                return_tensors='pt',
+            )
         
         for k, v in targets.items():
             if k == 'input_ids':
@@ -166,6 +191,15 @@ class ParadetoxDatasetForEval(Dataset):
                 truncation=True,
                 return_tensors='pt',
             )
+            
+        else:
+            inputs = self.tokenizer(
+                "Your task is to review the given toxic comment and convert it into a polite, neutral sentence.\nToxic comment: " + self.data[idx]['toxic'],
+                max_length=self.max_length,
+                padding='max_length',
+                truncation=True,
+                return_tensors='pt',
+            )
         
         tokenized = {k: v.squeeze(0).to(self.device) for k, v in inputs.items()}
 
@@ -195,10 +229,30 @@ def main(args):
         for ref in item['references']
     ]
     
-    tokenizer = T5Tokenizer.from_pretrained(args.base_model_name)
-    model = T5ForConditionalGeneration.from_pretrained(args.base_model_name)
-    model.to(device)
-    
+    if 't5' in args.base_model_name:        
+        tokenizer = T5Tokenizer.from_pretrained(args.base_model_name)
+        model = T5ForConditionalGeneration.from_pretrained(args.base_model_name)
+        model.to(device)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.base_model_name, padding_side='left', truncation_size='left')
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        model = AutoModelForCausalLM.from_pretrained(args.base_model_name, trust_remote_code=True, torch_dtype=torch.float16)
+        model.to(device)
+        
+        # lora tuning configuration
+        for name, param in model.named_parameters():
+            param.requires_grad = False
+        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj"]
+        config = LoraConfig(
+            target_modules=target_modules,
+            r=16,
+            lora_alpha=32,
+            lora_dropout=0.1
+        )
+        model = get_peft_model(model, config)
+        model.print_trainable_parameters()
+
     train_dataset = ParadetoxDatasetForTrain(train, tokenizer, device)
     valid_dataset = ParadetoxDatasetForEval(valid, tokenizer, device)
     test_dataset = ParadetoxDatasetForEval(test, tokenizer, device)
@@ -218,22 +272,32 @@ def main(args):
     
     for epoch in range(args.epochs):
         patience += 1
-        train_one_epoch(model, train_dataloader, optimizer, scheduler, criterion, device)
+        train_one_epoch(model, tokenizer, train_dataloader, optimizer, scheduler, criterion, device)
         valid_score = valid_one_epoch(model, valid_dataloader, tokenizer=tokenizer, device=device, raw_data=valid)
     
         if best_valid_score < valid_score:
             patience = 0
             best_valid_score = valid_score
-            torch.save(model.state_dict(), f'./checkpoints/best_{args.output_file_name}.pth')
+            
+            if isinstance(model, T5ForConditionalGeneration):
+                torch.save(model.state_dict(), f'./checkpoints/best_{args.output_file_name}.pth')
+            else:
+                model.save_pretrained(f'./checkpoints/best_{args.output_file_name}')
+                
             print(f'Best model saved (BLEU: {valid_score:.4f})')
             
         if patience > 10:
             print(f"Early stopping at epoch {epoch + 1}")
             break
     
-    model = T5ForConditionalGeneration.from_pretrained(args.base_model_name)
-    model.load_state_dict(torch.load(f'./checkpoints/best_{args.output_file_name}.pth'))
-    model.to(device)
+    if 't5' in args.base_model_name:
+        model = T5ForConditionalGeneration.from_pretrained(args.base_model_name)
+        model.load_state_dict(torch.load(f'./checkpoints/best_{args.output_file_name}.pth'))
+        model.to(device)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(args.base_model_name, trust_remote_code=True, torch_dtype=torch.float16)
+        model.load_adapter(Path(f'./checkpoints/best_{args.output_file_name}/'), adapter_name='lora')
+        model.to(device)
     
     inference(model, test_dataloader, args.output_file_name, tokenizer, test, device)
 
